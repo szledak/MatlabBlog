f121d399980882a3d555b0634fc1aca3b1db8d31
chakdeami <szledak@op.pl>
2015-09-17 18:49:49 +0200

    Add ListViewModel class

---
+++ MatlabBlog/Models/ListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MatlabBlog.Core.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MatlabBlog.Models
+{
+    public class ListViewModel
+    {
+        public IList<Post> Posts { get; private set; }
+        public int TotalPosts { get; private set; }
+    }
+}
