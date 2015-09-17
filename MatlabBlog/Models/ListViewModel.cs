ffc989fd1a4fb524f4ac78460540c14a0e6e0b4f
chakdeami <szledak@op.pl>
2015-09-17 19:12:07 +0200

    Add methods to BlogController and ListViewModel

--- MatlabBlog/Models/ListViewModel.cs
+++ MatlabBlog/Models/ListViewModel.cs
@@ -1,4 +1,5 @@
-﻿using MatlabBlog.Core.Objects;
+﻿using MatlabBlog.Core;
+using MatlabBlog.Core.Objects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,9 +7,57 @@ using System.Web;
 
 namespace MatlabBlog.Models
 {
+    /// <summary>
+    /// View model used for list view.
+    /// </summary>
+    /// <remarks>
+    /// Same view model is used to list posts for category, tag or search text.
+    /// </remarks>
     public class ListViewModel
     {
-        public IList<Post> Posts { get; private set; }
-        public int TotalPosts { get; private set; }
+        public ListViewModel(IBlogRepository blogRepository, int p)
+        {
+            Posts = blogRepository.Posts(p - 1, 10);
+            TotalPosts = blogRepository.TotalPosts();
+        }
+
+        public ListViewModel(IBlogRepository blogRepository, string text, string type, int p)
+        {
+            switch (type)
+            {
+                case "Category":
+                    Posts = blogRepository.PostsForCategory(text, p - 1, 10);
+                    TotalPosts = blogRepository.TotalPostsForCategory(text);
+                    Category = blogRepository.Category(text);
+                    break;
+
+                case "Tag":
+                    Posts = blogRepository.PostsForTag(text, p - 1, 10);
+                    TotalPosts = blogRepository.TotalPostsForTag(text);
+                    Tag = blogRepository.Tag(text);
+                    break;
+
+                default:
+                    Posts = blogRepository.PostsForSearch(text, p - 1, 10);
+                    TotalPosts = blogRepository.TotalPostsForSearch(text);
+                    Search = text;
+                    break;
+            }
+        }
+
+        public IList<Post> Posts
+        { get; private set; }
+
+        public int TotalPosts
+        { get; private set; }
+
+        public Category Category
+        { get; private set; }
+
+        public Tag Tag
+        { get; private set; }
+
+        public string Search
+        { get; private set; }
     }
 }
