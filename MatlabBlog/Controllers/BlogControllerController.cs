92387f170384a141c880f1f3e081f7128a57f1c0
chakdeami <szledak@op.pl>
2015-09-17 18:42:10 +0200

    Add BlogController

---
+++ MatlabBlog/Controllers/BlogControllerController.cs
@@ -0,0 +1,19 @@
+﻿using MatlabBlog.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MatlabBlog.Controllers
+{
+    public class BlogController
+    {
+        private readonly IBlogRepository _blogRepository;
+
+        public BlogController(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+    }
+}
