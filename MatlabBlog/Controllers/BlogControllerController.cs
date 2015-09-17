604458de09fc32714510012803f1b677c1999916
chakdeami <szledak@op.pl>
2015-09-17 18:48:44 +0200

    Add Posts method

--- MatlabBlog/Controllers/BlogControllerController.cs
+++ MatlabBlog/Controllers/BlogControllerController.cs
@@ -15,5 +15,23 @@ namespace MatlabBlog.Controllers
         {
             _blogRepository = blogRepository;
         }
+
+        public ViewResult Posts(int p = 1)
+        {
+            // pick latest 10 posts
+            var posts = _blogRepository.Posts(p - 1, 10);
+
+            var totalPosts = _blogRepository.TotalPosts();
+
+            var listViewModel = new ListViewModel
+            {
+                Posts = posts,
+                TotalPosts = totalPosts
+            };
+
+            ViewBag.Title = "Latest Posts";
+
+            return View("List", listViewModel);
+        }
     }
 }
