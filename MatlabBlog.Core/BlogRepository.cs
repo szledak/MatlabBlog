def15d22568b316076262aaeb554d8130115a5aa
chakdeami <szledak@op.pl>
2015-09-17 17:56:04 +0200

    Add BlogRepository class

---
+++ MatlabBlog.Core/BlogRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MatlabBlog.Core.Objects;
+using NHibernate;
+using NHibernate.Linq;
+
+namespace MatlabBlog.Core
+{
+    public class BlogRepository : IBlogRepository
+    {
+        // NHibernate object
+        private readonly ISession _session;
+
+        public IList<Post> Posts(int pageNo, int pageSize)
+        {
+            var posts = _session.Query<Post>()
+                .Where(p => p.Published)
+                .OrderByDescending(p => p.PostedOn)
+                .Skip(pageNo * pageSize)
+                .Take(pageSize)
+                .Fetch(p => p.Category)
+                .ToList();
+
+            var postIds = posts.Select(p => p.Id).ToList();
+
+            return _session.Query<Post>()
+                .Where(p => postIds.Contains(p.Id))
+                .OrderByDescending(p => p.PostedOn)
+                .FetchMany(p => p.Tags)
+                .ToList();
+        }
+
+        public int TotalPosts()
+        {
+            return _session.Query<Post>().Where(p => p.Published).Count();
+        }
+    }
+}
